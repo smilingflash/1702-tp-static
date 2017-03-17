cbdce20fb4f46feeaaaa0dd2dc16cf4a7e3228ab
Nitika Nadgar <nitiquita@gmail.com>
2017-03-17 14:41:32 -0400

    setting up

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var nunjucks = require('nunjucks');
+var models = require('./models')
+var db = require()
 
 var app = express();
 
@@ -11,12 +14,18 @@ app.use(bodyParser.json());
 
 app.use(morgan('dev'))
 
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+var env = nunjucks.configure('views',{noCache:true});
+
 app.use(function(err, req, res, next) {
 	console.error(err)
 	res.status(500)
 	res.send(err, err.stack)
 })
 
-app.listen(3000, function(){
-	console.log("Listening on 3000")
-})
+// app.listen(3000, function(){
+// 	console.log("Listening on 3000")
+// })
+
+//add db.sync
---
+++ models/index.js
@@ -0,0 +1,8 @@
+var Sequelize = require('sequelize');
+
+var db = new Sequelize('postgres://localhost:5432/tripPlanner', {
+    logging: false
+});
+
+
+module.exports = db;
